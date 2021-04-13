090b45cb1444f3979c2592db005854e3cbc10d63
kmxionghang <1364052798@qq.com>
2021-04-13 21:09:40 +0800

    vote only once

--- database/migrations/2021_04_13_110436_create_votes_table.php
+++ database/migrations/2021_04_13_110436_create_votes_table.php
@@ -22,6 +22,7 @@ class CreateVotesTable extends Migration
             $table->timestamps();
 
             $table->unique(['user_id','voted_id','voted_type','type']);
+            $table->unique(['user_id','voted_id','voted_type','type']);
         });
     }
 
