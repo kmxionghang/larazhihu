fc52ccdc83425994d5966dc879e511f057df8154
kmxionghang <1364052798@qq.com>
2021-04-13 14:40:13 +0800

    post answer

---
+++ tests/Feature/PostAnswerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Question;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostAnswerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test **/
+    public function user_can_post_an_answer_to_a_question()
+    {
+        $question = factory(Question::class)->create();
+        $user = factory(User::class)->create();
+
+        $response = $this->post("/questions/{$question->id}/answers", [
+            'user_id' => $user->id,
+            'content' => 'This is an answer'
+        ]);
+
+        $response->assertStatus(201);
+
+        $answer = $question->answers()->where('user_id',$user->id)->first();
+        $this->assertNotNull($answer);
+
+        $this->assertEquals(1,$question->answers()->count());
+    }
+}
