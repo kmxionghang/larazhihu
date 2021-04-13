5263861fe5ee6cbf969c01425233222be703a50f
kmxionghang <1364052798@qq.com>
2021-04-13 17:13:21 +0800

    delete answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Answer;
 use App\Question;
 use Illuminate\Http\Request;
 
@@ -26,6 +27,14 @@ class AnswersController extends Controller
         ]);
 
 //        return response()->json([], 201);
+        return back();
+    }
+
+
+    public function destroy(Answer $answer)
+    {
+        $this->authorize('delete', $answer);
+        $answer->delete();
         return back();
     }
 }
