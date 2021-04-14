b58a70f49c86facad0adcb0f2541cc118787a270
kmxionghang <1364052798@qq.com>
2021-04-14 16:33:55 +0800

    comment vote

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    use \App\Models\Traits\VoteTrait;
 
     protected $guarded = ['id'];
+
+    protected $appends = [
+        'upVotesCount',
+        'downVotesCount',
+    ];
 }
