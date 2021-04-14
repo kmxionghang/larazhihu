0416db46ed401f45681d651fa81ebe6d94705439
kmxionghang <1364052798@qq.com>
2021-04-14 11:14:21 +0800

    invite someone

--- app/Http/Controllers/PublishedQuestionsController.php
+++ app/Http/Controllers/PublishedQuestionsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Models\User;
+use App\Notifications\YouWereInvited;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -16,6 +18,18 @@ class PublishedQuestionsController extends Controller
     public function store(Question $question)
     {
         $this->authorize('update', $question);
+
+        preg_match_all('/@([^\s.]+)/',$question->content,$matches);
+
+        $names = $matches[1];
+
+        foreach ($names as $name){
+            $user = User::whereName($name)->first();
+
+            if($user){
+                $user->notify(new YouWereInvited($question));
+            }
+        }
 
         $question->publish();
     }
